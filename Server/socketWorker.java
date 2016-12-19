c6d6d1643ccd092dd7d9bb725db1098741bb41fe
Luca <luca.volturno@jcmaxwell.it>
2016-12-19 08:41:37 +0100

    Aggiunta di commenti
    Son stati aggiunti tutti i commenti in tutte le righe, per spiegare cosa accade nel programma

--- Server/socketWorker.java
+++ Server/socketWorker.java
@@ -25,44 +25,50 @@ class SocketWorker implements Runnable {
         
         BufferedReader in = null;
         PrintWriter out = null;
-        boolean contaNick = false;
+        boolean nickPresente = false; // Booleano che verifica se il nick è già stato inserito o meno
         
         try{
           // connessione con il socket per ricevere (in) e mandare(out) il testo
-          in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-          out = new PrintWriter(client.getOutputStream(), true);
+          in = new BufferedReader(new InputStreamReader(client.getInputStream())); // Stream di input, che viene RICEVUTO dal server, quindi spedito dal client
+          out = new PrintWriter(client.getOutputStream(), true); // Stream di output, che ESCE dal server
         } catch (IOException e) {
           System.out.println("Errore: in|out fallito");
           System.exit(-1);
         }
 
         String line = "";
-        int clientPort = client.getPort(); //il "nome" del mittente (client)
-        while(line != null){
-          try{
-              
-              if(line.equals("/listautenti") || line.equals("/listaUtenti"))
+      
+        int clientPort = client.getPort(); // Questa è la porta del socket del client. 
+      
+        while(line != null){ // Finchè line (stringa che viene riempita con l'input inserito dal client) è diverso da null, allora
+          try{ 
+           
+            // Se l'utente client, digita: /listautenti, allora verrà mostrata tutta la lista degli utenti connessi
+              if(line.equals("/listautenti") || line.equals("/listaUtenti")) 
               {
                   for(int i = 0; i < ServerTestoMultiThreaded.listaUtenti.size(); i++)
                   {
-                      out.println(ServerTestoMultiThreaded.listaUtenti.get(i).nickname);
+                      out.println(ServerTestoMultiThreaded.listaUtenti.get(i).nickname); // Per ogni indice, viene preso il nickname dalla lista utenti
                   }
               }
               
-              if(contaNick == false)
+              if(nickPresente == false)  // Condizione booleana, verifica se il nickname è stato già inserito o meno
               {
-                  nickname = in.readLine();
-                  out.println("Benvenuto: " + nickname);
-                  contaNick = true;
+                  nickname = in.readLine(); // Legge l'input dal clinet e lo assegna alla variabile nickname
+                
+                // -----------------------------DA MIGLIORARE-------------------------------
+                  out.println("Benvenuto: " + nickname); // Da un messaggio di benvenuto
+                //---------------------------------------------------------------------------
+                  nickPresente = true; // Il nickname è stato inserito quindi la variabile diventa true
               }
               
-              
-              
-              else
+              else // Altrimenti
               {
-                line = in.readLine();
+                line = in.readLine(); // Leggi l'input dal client
+                
                 //Manda lo stesso messaggio appena ricevuto con in aggiunta il "nome" del client
                 out.println("Server-->" + nickname + ">> " + line);
+                
                 //scrivi messaggio ricevuto su terminale
                 System.out.println(nickname + ">> " + line);
               }

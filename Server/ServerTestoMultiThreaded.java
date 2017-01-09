2ca5848192df2ead4d51e8354f1c9791e5232916
Luca <luca.volturno@jcmaxwell.it>
2017-01-09 08:24:22 +0100

    Spostato il file al branch maaster
    Avevo per sbaglio caricato il file nel mio branch, lo rimetto qui

--- Server/ServerTestoMultiThreaded.java
+++ Server/ServerTestoMultiThreaded.java
@@ -1,3 +1,4 @@
+
 /*
  * ServerTesto MultiThreaded.java Server che attende per richieste di connessioni da Clients
  * e li gestisce in modo contemporaneo generando un socket "worker" per ogni connessione.
@@ -10,9 +11,12 @@
  */
 import java.net.*;
 import java.io.*;
+import java.util.LinkedList;
 
-public class ServerTestoMultiThreaded {
-
+public class ServerTestoMultiThreaded
+{
+    static LinkedList<SocketWorker> listaUtenti = new LinkedList<SocketWorker>(); // Creo una lista di socket, da cui estrarrò il nickname assegnato ad ogni socket
+    
     public static void main(String[] args) {
 
         if (args.length != 1) {
@@ -32,6 +36,7 @@ public class ServerTestoMultiThreaded {
                 try {
                     //server.accept returns a client connection
                     w = new SocketWorker(server.accept());
+                    listaUtenti.add(w); // Inserisco il socket nella lista degli utenti connessi
                     Thread t = new Thread(w);
                     t.start();
                 } catch (IOException e) {
